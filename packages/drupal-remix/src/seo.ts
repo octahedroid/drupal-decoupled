8e14a81369359c2d31f7683eb96d72818770b5c6
enZane <57777002+enZane@users.noreply.github.com>
2023-05-25 17:46:25 -0600

    :sparkles: Add type definitions (#4)

--- packages/drupal-remix/src/seo.ts
+++ packages/drupal-remix/src/seo.ts
@@ -1,3 +1,47 @@
+type MetaTagUnion = (MetaTagLink | MetaTagValue | MetaTagProperty) & {
+  __isUnion?: true;
+};
+
+interface MetaTagLink {
+  tag: string;
+  attributes: MetaTagLinkAttributes;
+  __typename: "MetaTagLink";
+}
+
+type MetaTag = (MetaTagLink | MetaTagValue | MetaTagProperty) & {
+  __isUnion?: true;
+};
+
+interface MetaTagLinkAttributes {
+  rel?: string;
+  href?: string;
+  __typename: "MetaTagLinkAttributes";
+}
+
+interface MetaTagValue {
+  tag: string;
+  attributes: MetaTagValueAttributes;
+  __typename: "MetaTagValue";
+}
+
+interface MetaTagValueAttributes {
+  name?: string;
+  content?: string;
+  __typename: "MetaTagValueAttributes";
+}
+
+interface MetaTagProperty {
+  tag: string;
+  attributes: MetaTagPropertyAttributes;
+  __typename: "MetaTagProperty";
+}
+
+interface MetaTagPropertyAttributes {
+  property?: string;
+  content?: string;
+  __typename: "MetaTagPropertyAttributes";
+}
+
 export const metaTags = (tags: MetaTagUnion[]) => {
   const title = tags.find(
     (tag: MetaTagUnion) =>
