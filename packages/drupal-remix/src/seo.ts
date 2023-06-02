c59937e6d9dee4f1f58dc7d18d3bda4e358bd3a3
enZane <thunderstudios2@gmail.com>
2023-06-02 16:39:50 -0600

    [Drupal-Remix] :sparkles: Add Override to metatags

--- packages/drupal-remix/src/seo.ts
+++ packages/drupal-remix/src/seo.ts
@@ -42,39 +42,115 @@ interface MetaTagPropertyAttributes {
   __typename: "MetaTagPropertyAttributes";
 }
 
-export const metaTags = (tags: MetaTagUnion[]) => {
+type OverrideKind = "replace" | "override";
+
+type OverrideOptions = {
+  kind: OverrideKind;
+  pattern?: string;
+  replacement: string;
+};
+
+type MetaTagOverrides = {
+  [key in MetaTagUnion["__typename"]]?: {
+    [key: string]: OverrideOptions;
+  };
+};
+
+const DEFAULT_TAGS = [
+  { charSet: "utf-8" },
+  { name: "viewport", content: "width=device-width,initial-scale=1" },
+];
+
+export const metaTags = ({
+  tags,
+  defaultTags = [],
+  metaTagOverrides,
+}: {
+  tags: MetaTag[];
+  defaultTags?: Array<{ [key: string]: string | undefined }>;
+  metaTagOverrides?: MetaTagOverrides;
+}) => {
   const title = tags.find(
     (tag: MetaTagUnion) =>
       tag.__typename === "MetaTagValue" && tag.attributes.name === "title"
   ) as MetaTagValue;
 
-  const defaultTags = [
-    { title: title.attributes.content },
-    { charSet: "utf-8" },
-    { name: "viewport", content: "width=device-width,initial-scale=1" },
-  ];
-
   const meta = tags.map((tag: MetaTagUnion) => {
+    const overrideTag = metaTagOverrides?.[tag.__typename];
+
     if (tag.__typename === "MetaTagLink") {
+      const { rel, href } = tag.attributes;
+      const willOverrideTag = overrideTag?.[rel!];
+      if (!willOverrideTag) {
+        return {
+          tagName: "link",
+          rel,
+          href,
+        };
+      }
+
+      const { kind, pattern, replacement } = willOverrideTag;
+
+      const hrefOverride =
+        kind === "override" ? replacement : href?.replace(pattern!, replacement);
+
       return {
         tagName: "link",
-        rel: tag.attributes.rel,
-        href: tag.attributes.href,
+        rel,
+        href: hrefOverride,
       };
     }
     if (tag.__typename === "MetaTagProperty") {
+      const { property, content } = tag.attributes;
+      const willOverrideTag = overrideTag?.[property!];
+      if (!willOverrideTag) {
+        return {
+          property: tag.attributes.property,
+          content: tag.attributes.content,
+        };
+      }
+
+      const { kind, pattern, replacement } = willOverrideTag;
+
+      const contentOverride =
+        kind === "override"
+          ? replacement
+          : content?.replace(pattern!, replacement);
+
       return {
-        property: tag.attributes.property,
-        content: tag.attributes.content,
+        property,
+        content: contentOverride,
       };
     }
     if (tag.__typename === "MetaTagValue") {
+      const { name, content } = tag.attributes;
+      const willOverrideTag = overrideTag?.[name!];
+
+      if (!willOverrideTag) {
+        return {
+          name: tag.attributes.name!,
+          content: tag.attributes.content!,
+        };
+      }
+
+      const { kind, pattern, replacement } = willOverrideTag;
+
+      const contentOverride =
+        kind === "override"
+          ? replacement
+          : content?.replace(pattern!, replacement);
+
       return {
-        name: tag.attributes.name!,
-        content: tag.attributes.content!,
+        name,
+        content: contentOverride,
       };
     }
   });
 
-  return [...defaultTags, ...meta];
+  return [
+    ...DEFAULT_TAGS,
+    ...defaultTags,
+    title ? { title: title.attributes.content } : {},
+    ...meta,
+  ];
 };
