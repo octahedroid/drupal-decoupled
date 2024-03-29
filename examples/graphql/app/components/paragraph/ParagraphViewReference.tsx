68ddacddfdffdc94cd31934b38234423ad770b84
Jesus Manuel Olivas (jmolivas) <jesus.olivas@gmail.com>
2024-03-29 00:40:51 -0700

    ♻️ fix (drupal-remix-graphql): fix TS errors

--- examples/graphql/app/components/paragraph/ParagraphViewReference.tsx
+++ examples/graphql/app/components/paragraph/ParagraphViewReference.tsx
@@ -1,18 +1,18 @@
 import { FragmentOf, readFragment } from "gql.tada";
 import { ParagraphViewReferenceFragment } from "~/graphql/fragments/paragraph";
 import BlogTeaser from "~/components/view/BlogTeaser";
-
+import { NodeArticleTeaserFragment } from "~/graphql/fragments/node";
 interface ParagraphViewReferenceProps {
   paragraph: FragmentOf<typeof ParagraphViewReferenceFragment>
 }
 
 export default function ParagraphViewReference({ paragraph } : ParagraphViewReferenceProps) {
+
   const { reference: { view, display, results } } = readFragment(ParagraphViewReferenceFragment, paragraph);
   if (view === 'blog' && display === 'blog_teaser') {
-    
     return (
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
-        <BlogTeaser results={results} />
+        <BlogTeaser results={results as Array<FragmentOf<typeof NodeArticleTeaserFragment>>} />
       </div>
     );
   }
--- examples/graphql/app/components/view/BlogTeaser.tsx
+++ examples/graphql/app/components/view/BlogTeaser.tsx
@@ -1,5 +1,5 @@
 import { FragmentOf, readFragment } from "gql.tada";
-import { NodeArticleTeaserFragment } from "~/graphql/fragments/node";
+import { NodeArticleFragment, NodeArticleTeaserFragment } from "~/graphql/fragments/node";
 import NodeArticleTeaser from "~/components/node/NodeArticleTeaser";
 
 interface BlogTeaserProps {
@@ -17,7 +17,7 @@ export default function BlogTeaser(
           const node = readFragment(NodeArticleTeaserFragment, result);
           return (
             // @todo fix use of NodeArticleTeaserFragment
-            <NodeArticleTeaser key={index} node={node}  />
+            <NodeArticleTeaser key={index} node={node as unknown as FragmentOf<typeof NodeArticleFragment>}  />
           );
         })
       }
