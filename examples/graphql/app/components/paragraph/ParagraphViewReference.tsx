01f45939a15caee9749e01fb056ceb4279d36fcb
Jesus Manuel Olivas (jmolivas) <jesus.olivas@gmail.com>
2024-03-29 00:44:03 -0700

    ♻️ fix (drupal-remix-graphql): fix comments

--- examples/graphql/app/components/paragraph/ParagraphViewReference.tsx
+++ examples/graphql/app/components/paragraph/ParagraphViewReference.tsx
@@ -11,6 +11,7 @@ export default function ParagraphViewReference({ paragraph } : ParagraphViewRefe
   const { reference: { view, display, results } } = readFragment(ParagraphViewReferenceFragment, paragraph);
   if (view === 'blog' && display === 'blog_teaser') {
     return (
+      // @todo fix use of NodeArticleTeaserFragment
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         <BlogTeaser results={results as Array<FragmentOf<typeof NodeArticleTeaserFragment>>} />
       </div>
@@ -21,7 +22,7 @@ export default function ParagraphViewReference({ paragraph } : ParagraphViewRefe
     <>
       <h2>ParagraphViewReference</h2>
       <pre>
-        {JSON.stringify( {view, display, results}, null, 2)}
+        {JSON.stringify({ view, display, results }, null, 2)}
       </pre>
     </>
   );
