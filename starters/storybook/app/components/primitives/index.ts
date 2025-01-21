c04a50e7aaa185a744f89104dc92ae76e3e3eaf7
Omar Aguirre <omersguchigu@gmail.com>
2025-01-21 17:28:55 -0600

    ♻️ feat: Adding Testimonial component

--- starters/storybook/app/components/primitives/index.ts
+++ starters/storybook/app/components/primitives/index.ts
@@ -1,5 +1,5 @@
 export { Accordion } from '~/components/primitives/Accordion/Accordion'
-export { Avatar } from '~/components/primitives/Avatar/Avatar'
+export { Avatar, type AvatarProps } from '~/components/primitives/Avatar/Avatar'
 export { Badge } from '~/components/primitives/Badge/Badge'
 export { Button, type ButtonProps } from '~/components/primitives/Button/Button'
 export { Link, type LinkProps } from '~/components/primitives/Link/Link'
