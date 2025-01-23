20526fca6ed43db06399160f849199b3990f8e84
Omar Aguirre <omers@octahedroid.com>
2025-01-23 17:08:47 -0600

    ♻️ feat: Components refactor (#124)
    * ♻️ feat: adding latest shadcn changes and starting refactor of components
    
    * ♻️ feat: Adding RichText and FAQ component
    
    * 🩹 fix: removing unnecessary union
    
    * ♻️ feat: Adding Testimonial component
    
    * ♻️ feat: Adding Image, SimpleCard and TeaserCard components
    
    * ♻️ feat: Adding CardGroup
    
    * ♻️ feat: Adding Article component
    
    * ♻️ feat: Adding NavigationMenu, Header and Footer
    
    * ♻️ feat: Adding Article and Page pages
    
    * 🧑‍💻 feat: adding vscode workspace
    
    * 🏷️ fix: Default props and some typing
    
    * 📝 feat: Story renames
    
    * 🎨 feat: prettier all the things
    
    * 🏷️ feat: typing feedback
    
    * 🏷️ feat: Update typing
    
    * ⬆️ feat: upgrading storybook
    
    * 🩹 fix: heading validation

---
+++ starters/storybook/app/components/blocks/Hero/Hero.tsx
@@ -0,0 +1,77 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import type { ComponentPropsWithoutRef } from 'react'
+import {
+  Button,
+  type ButtonProps,
+  Image,
+  type ImageProps,
+} from '~/components/primitives'
+import { cn } from '~/lib/utils'
+
+const heroVariants = cva('w-full px-4 py-8 md:py-16 lg:py-24', {
+  variants: {},
+  defaultVariants: {},
+})
+
+export interface HeroProps
+  extends ComponentPropsWithoutRef<'div'>,
+    VariantProps<typeof heroVariants> {
+  heading: string
+  description: string
+  image?: ImageProps
+  actions?: ButtonProps[]
+}
+
+export const Hero = ({
+  className,
+  heading,
+  description,
+  image,
+  actions,
+  ...props
+}: HeroProps) => {
+  return (
+    <div className={cn(heroVariants(), className)} {...props}>
+      <div className="container mx-auto grid items-center gap-8 lg:grid-cols-2">
+        <div className="space-y-4 text-center lg:text-left">
+          <h1 className="mb-6 text-4xl font-bold text-gray-900 dark:text-gray-100 sm:text-5xl md:text-6xl">
+            {heading}
+          </h1>
+          <p className="mx-auto max-w-2xl text-lg text-muted-foreground lg:mx-0">
+            {description}
+          </p>
+          {actions && actions.length > 0 && (
+            <div className="flex flex-wrap justify-center gap-4 lg:justify-start">
+              {actions.slice(0, 2).map(({ variant, ...actionProps }, index) => (
+                <Button
+                  variant={index === 1 ? 'outline' : variant || 'default'}
+                  {...actionProps}
+                  key={index}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="order-first mx-auto w-full max-w-lg lg:order-last lg:max-w-none">
+          {image && (
+            <Image {...image} className={cn('h-auto w-full object-cover')} />
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Hero.defaultProps = {
+  heading: 'Welcome to our Hero Section',
+  description:
+    'This is a default description. You can customize it to fit your needs.',
+  image: {
+    src: '/placeholders/drupal-decoupled/landscape-large.png',
+    alt: 'Default hero image',
+  },
+  actions: [
+    { text: 'Get Started', href: '#', variant: 'default', internal: true },
+    { text: 'Learn More', href: '#', variant: 'outline' },
+  ],
+} satisfies HeroProps
--- starters/storybook/app/components/ui/FAQ/index.ts
+++
@@ -1,1 +0,0 @@
-export { FAQ } from './FAQ'
