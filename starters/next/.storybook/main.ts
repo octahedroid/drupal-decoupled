ce9de95b4f75a17a2a504eb242ec3be5a229f503
Omar Aguirre <omersguchigu@gmail.com>
2025-01-24 16:28:11 -0600

    🔥 feat: removing chromatic dependency

--- starters/next/.storybook/main.ts
+++ starters/next/.storybook/main.ts
@@ -18,7 +18,6 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-interactions'),
   ],
   framework: {
