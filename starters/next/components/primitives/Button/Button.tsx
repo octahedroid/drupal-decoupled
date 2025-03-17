f3dd70e2cb46910fc7795d6e7943fd328fc1684a
Omar Aguirre <omers@octahedroid.com>
2025-03-17 14:07:17 -0600

    ♻️feat: children for button icon (#143)

--- starters/next/components/primitives/Button/Button.tsx
+++ starters/next/components/primitives/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { LucideIcon } from 'lucide-react'
+import { ReactNode } from 'react'
 import { Link } from '@/components/primitives'
 import {
   Button as ShadcnButton,
@@ -9,16 +9,17 @@ export interface ButtonProps extends Omit<ShadcnButtonProps, 'asChild'> {
   href: string
   internal?: boolean
   text: string
-  icon?: LucideIcon
+  children?: ReactNode
 }
 
-export const Button = ({ href, text, internal, ...props }: ButtonProps) => {
-  const content = (
-    <>
-      {text}
-      {props.icon && <props.icon className="ml-2 h-4 w-4" />}
-    </>
-  )
+export const Button = ({
+  href,
+  text,
+  internal,
+  children,
+  ...props
+}: ButtonProps) => {
+  const content = children ? (text ? <>{text} {children}</> : children) : (text && <>{text}</>)
   if (href) {
     return (
       <Link internal={internal} href={href}>
