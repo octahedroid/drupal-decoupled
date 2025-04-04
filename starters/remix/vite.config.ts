8b195ac47d0c059325c4e11693c307877ffd354c
enZane <ezequiel@octahedroid.com>
2025-04-03 19:00:24 -0600

    ✨ feat: Update remix and set up feature flags

--- starters/remix/vite.config.ts
+++ starters/remix/vite.config.ts
@@ -17,8 +17,9 @@ export default defineConfig({
           v3_fetcherPersist: true,
           v3_relativeSplatPath: true,
           v3_throwAbortReason: true,
-          // v3_singleFetch: true,
-          // v3_lazyRouteDiscovery: true,
+          v3_singleFetch: true,
+          v3_lazyRouteDiscovery: true,
+          unstable_optimizeDeps: true,
         },
         ignoredRouteFiles: ['**/*'],
         routes: async (defineRoutes) => {
