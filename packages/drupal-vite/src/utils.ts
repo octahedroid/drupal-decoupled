993566ad11af665f2a1c9c009dfa40600fc2d087
enZane <ezequiel@octahedroid.com>
2025-06-17 16:45:41 -0600

    ✨ feat: Support cloudflare worker

--- packages/drupal-vite/src/utils.ts
+++ packages/drupal-vite/src/utils.ts
@@ -1,14 +1,31 @@
-export const resolveValue = (
+async function importWrangler() {
+  try {
+    const module =  await import('wrangler');
+    return module;
+  } catch (e) {
+    return false;
+  }
+}
+
+export const resolveValue = async (
   configValue: string | undefined,
   defaultEnvKey: string
-): string | undefined => {
-  if (configValue && process.env[configValue]) {
-    return process.env[configValue];
+): Promise<string | undefined> => {
+  let envAccess: NodeJS.ProcessEnv | Record<string, unknown> = process.env;
+  const wrangler = await importWrangler();
+  if(wrangler) {
+    const { getPlatformProxy } = wrangler;
+    const platformProxy = await getPlatformProxy();
+    envAccess = platformProxy.env;
+  }
+
+  if (configValue && envAccess[configValue]) {
+    return envAccess[configValue] as string;
   }
 
   if (configValue) {
     return configValue;
   }
 
-  return process.env[defaultEnvKey];
+  return envAccess[defaultEnvKey] as string | undefined;
 };
